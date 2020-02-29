8da7ec91b49c14a3f178c0bd68b0bf5371c7878d
kylerjohnson26 <kylerljohnson@icloud.com>
2020-02-29 16:03:38 -0500

    adding processor variable with fallback for port, removing app service, addressing deprecation warnings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 import { GenresModule } from './genres/genres.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/book-inventory'),
+    MongooseModule.forRoot(
+      'mongodb://127.0.0.1:27017/book-inventory', 
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    ),
     BooksModule,
     GenresModule
   ],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
